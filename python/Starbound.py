542c2b99e72927f4455e806661b5020ff7a731da
The Temportalist <the.country.gamer@gmail.com>
2016-08-02 23:29:57 +0000

    Add space for function args in Stabound install

--- python/Starbound.py
+++ python/Starbound.py
@@ -54,7 +54,7 @@ class Server(Base.Server):
 	def install(self, **kwargs):
 		# Replaces the directory in the install script
 		self.replaceFileSubstring("install.steam", "%DIRECTORY%", self.dirRun)
-		os.system(Config.getSteamShell() + "+runscript " + os.path.join(self.dirRun, "install.steam"))
+		os.system(Config.getSteamShell() + " +runscript " + os.path.join(self.dirRun, "install.steam"))
 	
 	def replaceFileSubstring(self, filePath, option, string):
 		with open(filePath, "wt") as fout:
